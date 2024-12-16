7242d51cc2e448e6e8b9d82fa7168b97c556cf01
Madhan Kumar <madhankumar4195@gmail.com>
2024-12-16 22:14:31 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById('root')).render(
 //  <StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <GoogleOAuthProvider clientId='832458855190-3ittnsqdkn44mkbo6jm3d0v65iim1b1a.apps.googleusercontent.com'>
+        <GoogleOAuthProvider clientId=YOUT_CLIENT_ID>
           <App />
         </GoogleOAuthProvider>
       </PersistGate>
